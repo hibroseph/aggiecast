c442f23863b3d756e01a7aaad56a21d1d85b98f5
hibroseph <iwillridgley@gmail.com>
2018-11-16 21:26:16 -0700

    added sliding tab for main layout

--- app/src/main/java/usu/hack/aggiecast/MainActivity.java
+++ app/src/main/java/usu/hack/aggiecast/MainActivity.java
@@ -1,13 +1,20 @@
 package usu.hack.aggiecast;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+//    String TAG = this.getLocalClassName();
+    String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
